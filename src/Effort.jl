4334da61b6c69cbc1321b93429f73090e67fb6c0
marcobonici <bonici.marco@gmail.com>
2022-12-01 11:09:28 +0100

    Added better As rescaling

--- src/Effort.jl
+++ src/Effort.jl
@@ -27,19 +27,44 @@ end
 
 abstract type AbstractComponentEmulators end
 
-@kwdef mutable struct ComponentEmulator <: AbstractComponentEmulators
+@kwdef mutable struct P11Emulator <: AbstractComponentEmulators
     TrainedEmulator::AbstractTrainedEmulators
     kgrid::Array
     InMinMax::Matrix{Float64} = zeros(8,2)
     OutMinMax::Array{Float64} = zeros(2499,2)
 end
 
-function compute_component(input_params, comp_emu::ComponentEmulator)
+@kwdef mutable struct PloopEmulator <: AbstractComponentEmulators
+    TrainedEmulator::AbstractTrainedEmulators
+    kgrid::Array
+    InMinMax::Matrix{Float64} = zeros(8,2)
+    OutMinMax::Array{Float64} = zeros(2499,2)
+end
+
+@kwdef mutable struct PctEmulator <: AbstractComponentEmulators
+    TrainedEmulator::AbstractTrainedEmulators
+    kgrid::Array
+    InMinMax::Matrix{Float64} = zeros(8,2)
+    OutMinMax::Array{Float64} = zeros(2499,2)
+end
+
+function compute_component(input_params, comp_emu::AbstractComponentEmulators)
     input = deepcopy(input_params)
     maximin_input!(input, comp_emu.InMinMax)
     output = Array(run_emulator(input, comp_emu.TrainedEmulator))
     inv_maximin_output!(output, comp_emu.OutMinMax)
-    output .*= exp(input_params[1]-3.)
+    As = exp(input_params[1])*1e-10
+    output .*= As
+    return reshape(output, Int(length(output)/length(comp_emu.kgrid)), :)
+end
+
+function compute_component(input_params, comp_emu::PloopEmulator)
+    input = deepcopy(input_params)
+    maximin_input!(input, comp_emu.InMinMax)
+    output = Array(run_emulator(input, comp_emu.TrainedEmulator))
+    inv_maximin_output!(output, comp_emu.OutMinMax)
+    As = exp(input_params[1])*1e-10
+    output .*= As^2
     return reshape(output, Int(length(output)/length(comp_emu.kgrid)), :)
 end
 
@@ -50,9 +75,9 @@ end
 abstract type AbstractPℓEmulators end
 
 @kwdef mutable struct PℓEmulator <: AbstractPℓEmulators
-    P11::AbstractComponentEmulators
-    Ploop::AbstractComponentEmulators
-    Pct::AbstractComponentEmulators
+    P11::P11Emulator
+    Ploop::PloopEmulator
+    Pct::PctEmulator
 end
 
 function ComputePℓ(cosmology, bs, f, cosmoemu::AbstractPℓEmulators)
