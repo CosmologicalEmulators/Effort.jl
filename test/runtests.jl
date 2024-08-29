aca5b0902fd193f82f8dbcccc564054f6de92848
marcobonici <bonici.marco@gmail.com>
2024-08-29 13:43:47 -0400

    Updating CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,19 +18,12 @@ k_test = Array(LinRange(0,200, 40))
 weights = SimpleChains.init_params(mlpd)
 inminmax = rand(6,2)
 outminmax = rand(40,2)
-#npzwrite("emu/l.npy", k_test)
-#npzwrite("emu/weights.npy", weights)
-#npzwrite("emu/inminmax.npy", inminmax)
-#npzwrite("emu/outminmax.npy", outminmax)
+a, Ωγ0, Ωc0, Ωb0, mν, h, w0, wa = [1., 1e-5, 0.25, 0.05, 0., 0.67, -1, 0.]
 emu = Effort.SimpleChainsEmulator(Architecture = mlpd, Weights = weights)
 
 effort_emu = Effort.P11Emulator(TrainedEmulator = emu, kgrid=k_test, InMinMax = inminmax,
                                 OutMinMax = outminmax)
 
 @testset "Effort tests" begin
-    cosmo = ones(6)
-    cosmo_vec = ones(6,6)
-    output = Effort.get_component(cosmo,  effort_emu)
-    output_vec = Effort.get_component(cosmo_vec, effort_emu)
-    @test isapprox(output_vec[:,1], output)
+    @test isapprox(Effort._H_a(a, Ωγ0, Ωc0, Ωb0, mν, h, w0, wa), h*100)
 end
