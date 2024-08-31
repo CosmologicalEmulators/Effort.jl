bc4c4cc2778c937f62d966e864ab44a8727bddbe
marcobonici <bonici.marco@gmail.com>
2024-08-31 18:24:45 -0400

    Fixing tests

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,10 +1,2 @@
-function ChainRulesCore.rrule(::typeof(_simpson_weights), n)
-    Y = _simpson_weights(n)
-    function inv_maximin_pullback(Ȳ)
-        return NoTangent(), NoTangent()
-    end
-    return Y, inv_maximin_pullback
-end
-
 @non_differentiable LinRange(a,b,n)
 @non_differentiable _transformed_weights(quadrature_rule, order, a,b)
