00ec119995491fe1a1b3f78b161fef3cad33eba4
marcobonici <bonici.marco@gmail.com>
2024-11-06 11:50:02 -0500

    Added commands for the velocileptor bias contractions

--- src/eft_commands.jl
+++ src/eft_commands.jl
@@ -22,7 +22,7 @@ function get_Pℓ(cosmology::Array, bs::Array, f, cosmoemu::AbstractBinEmulators
 end
 
 function sum_Pℓ_components(P11_comp_array::AbstractArray{T}, Ploop_comp_array,
-    Pct_comp_array, bs, f) where {T}
+    Pct_comp_array, bs, f::Number) where {T}
     b1, b2, b3, b4, b5, b6, b7 = bs
 
     b11 = Array([ b1^2, 2*b1*f, f^2])
@@ -39,6 +39,18 @@ function sum_Pℓ_components(P11_comp_array::AbstractArray{T}, Ploop_comp_array,
     Pℓ = P11_array .+ Ploop_array .+ Pct_array
 
     return Pℓ
+end
+
+function sum_Pℓ_components(P11_comp_array::AbstractArray, Ploop_comp_array::AbstractArray,
+    Pct_comp_array::AbstractArray, Sn_comp_array::AbstractArray, biases::AbstractArray,)
+    b1, b2, b3, bs, alpha0, alpha2, alpha4, alpha6, sn, sn2, sn4 = biases
+    bias_11 = [1, b1, b1^2]
+    bias_loop = [b2, b1*b2, b2^2, bs, b1*bs, b2*bs, bs^2, b3, b1*b3]
+    bias_ct = [alpha0, alpha2, alpha4, alpha6]
+    sn_terms = [sn, sn2, sn4]
+
+    return @. P11_comp_array*bias_11 + Ploop_comp_array*bias_loop + Pct_comp_array*bias_ct +
+              Sn_comp_array*sn_terms
 end
 
 function bias_multiplication!(input_array, bias_array, Pk_input)
