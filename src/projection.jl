6622ae3ff6378a97637705e1768aa08425bc1bbf
marcobonici <bonici.marco@gmail.com>
2024-12-10 12:30:41 -0500

    Fixing typo

--- src/projection.jl
+++ src/projection.jl
@@ -243,6 +243,6 @@ function apply_AP(k::Array, mono::Array, quad::Array, q_par, q_perp;
     return result
 end
 
-function window_convolution(W::AbstractArray{NUmber, 4}, v::AbstractArray{NUmber, 2})
+function window_convolution(W::AbstractArray{Number, 4}, v::AbstractArray{Number, 2})
     return @tullio C[i,k] := W[i,j,k,l] * v[j,l]
 end
