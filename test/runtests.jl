f29aa7bd49d3af9eccf236b702dc3e75ae2a0d35
marcobonici <bonici.marco@gmail.com>
2024-12-10 12:56:23 -0500

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,7 +24,7 @@ k_test = Array(LinRange(0,200, 40))
 weights = SimpleChains.init_params(mlpd)
 inminmax = rand(6,2)
 outminmax = rand(40,2)
-a, Ωγ0, Ωm0, mν, h, w0, wa = [1., 1e-5, 0.3, 0.06, 0.67, -1.1, 0.2]
+a, Ωcb0, mν, h, w0, wa = [1., 0.3, 0.06, 0.67, -1.1, 0.2]
 z = Array(LinRange(0., 3., 100))
 
 emu = Effort.SimpleChainsEmulator(Architecture = mlpd, Weights = weights)
@@ -34,7 +34,7 @@ postprocessing = (input, output, D, Pkemu) -> output
 effort_emu = Effort.P11Emulator(TrainedEmulator = emu, kgrid=k_test, InMinMax = inminmax,
                                 OutMinMax = outminmax, Postprocessing = postprocessing)
 
-x = [Ωm0, h, mν, w0, wa]
+x = [Ωcb0, h, mν, w0, wa]
 
 n = 64
 x1 = vcat([0.], sort(rand(n-2)), [1.])
@@ -50,23 +50,23 @@ function di_spline(y,x,xn)
 end
 
 function D_z_x(z, x)
-    Ωm0, h, mν, w0, wa = x
-    sum(Effort._D_z(z, Ωm0, h; mν =mν, w0=w0, wa=wa))
+    Ωcb0, h, mν, w0, wa = x
+    sum(Effort._D_z(z, Ωcb0, h; mν =mν, w0=w0, wa=wa))
 end
 
 function f_z_x(z, x)
-    Ωm0, h, mν, w0, wa = x
-    sum(Effort._f_z(z, Ωm0, h; mν =mν, w0=w0, wa=wa))
+    Ωcb0, h, mν, w0, wa = x
+    sum(Effort._f_z(z, Ωcb0, h; mν =mν, w0=w0, wa=wa))
 end
 
 function r_z_x(z, x)
-    Ωm0, h, mν, w0, wa = x
-    sum(Effort._r_z(z, Ωm0, h; mν =mν, w0=w0, wa=wa))
+    Ωcb0, h, mν, w0, wa = x
+    sum(Effort._r_z(z, Ωcb0, h; mν =mν, w0=w0, wa=wa))
 end
 
 function r_z_check_x(z, x)
-    Ωm0, h, mν, w0, wa = x
-    sum(Effort._r_z_check(z, Ωm0, h; mν =mν, w0=w0, wa=wa))
+    Ωcb0, h, mν, w0, wa = x
+    sum(Effort._r_z_check(z, Ωcb0, h; mν =mν, w0=w0, wa=wa))
 end
 
 myx = Array(LinRange(0., 1., 100))
@@ -79,10 +79,10 @@ q_perp = 0.6
 x3 = Array(LinRange(-1., 1., 100))
 
 @testset "Effort tests" begin
-    @test isapprox(Effort._H_a(a, Ωγ0, Ωm0, mν, h, w0, wa), h*100)
-    @test isapprox(Effort._E_a(a, Ωm0, h), 1.)
-    @test isapprox(Effort._D_z_old(z, Ωm0, h), Effort._D_z(z, Ωm0, h), rtol=1e-9)
-    @test isapprox(Effort._f_z_old(0.4, Ωm0, h), Effort._f_z(0.4, Ωm0, h)[1], rtol=1e-9)
+    @test isapprox(Effort._H_a(a, Ωcb0, mν, h, w0, wa), h*100)
+    @test isapprox(Effort._E_a(a, Ωcb0, h), 1.)
+    @test isapprox(Effort._D_z_old(z, Ωcb0, h), Effort._D_z(z, Ωcb0, h), rtol=1e-9)
+    @test isapprox(Effort._f_z_old(0.4, Ωcb0, h), Effort._f_z(0.4, Ωcb0, h)[1], rtol=1e-9)
     @test isapprox(Zygote.gradient(x->D_z_x(z, x), x)[1], ForwardDiff.gradient(x->D_z_x(z, x), x), rtol=1e-5)
     @test isapprox(grad(central_fdm(5,1), x->D_z_x(z, x), x)[1], ForwardDiff.gradient(x->D_z_x(z, x), x), rtol=1e-5)
     @test isapprox(Zygote.gradient(x->f_z_x(z, x), x)[1], ForwardDiff.gradient(x->f_z_x(z, x), x), rtol=1e-5)
@@ -90,7 +90,7 @@ x3 = Array(LinRange(-1., 1., 100))
     @test isapprox(grad(central_fdm(5,1), x->r_z_x(3., x), x)[1], ForwardDiff.gradient(x->r_z_x(3., x), x), rtol=1e-7)
     @test isapprox(Zygote.gradient(x->r_z_x(3., x), x)[1], ForwardDiff.gradient(x->r_z_x(3., x), x), rtol=1e-6)
     @test isapprox(Zygote.gradient(x->r_z_x(3., x), x)[1], Zygote.gradient(x->r_z_check_x(3., x), x)[1], rtol=1e-7)
-    @test isapprox(Effort._r_z(3., Ωm0, h; mν =mν, w0=w0, wa=wa), Effort._r_z_check(3., Ωm0, h; mν =mν, w0=w0, wa=wa), rtol=1e-6)
+    @test isapprox(Effort._r_z(3., Ωcb0, h; mν =mν, w0=w0, wa=wa), Effort._r_z_check(3., Ωcb0, h; mν =mν, w0=w0, wa=wa), rtol=1e-6)
     @test isapprox(Effort._quadratic_spline(y, x1, x2), di_spline(y, x1, x2), rtol=1e-9)
     @test isapprox(ForwardDiff.gradient(y->sum(Effort._quadratic_spline(y,x1,x2)), y), Zygote.gradient(y->sum(Effort._quadratic_spline(y,x1,x2)), y)[1], rtol=1e-6)
     @test isapprox(ForwardDiff.gradient(x1->sum(Effort._quadratic_spline(y,x1,x2)), x1), Zygote.gradient(x1->sum(Effort._quadratic_spline(y,x1,x2)), x1)[1], rtol=1e-6)
