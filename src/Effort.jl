d088adaeaeff5d85643c09dcac9e8511d2a15e7a
Marco Bonici <bonici.marco@gmail.com>
2025-08-03 15:20:00 +0200

    Updated dependencies

--- src/Effort.jl
+++ src/Effort.jl
@@ -24,12 +24,12 @@ using Zygote: @adjoint
 const c_0 = 2.99792458e5
 
 function __init__()
-    min_y = _get_y(0,0) #obvious, I knowadd OrdinaryDiffEqTsit5
-    max_y = _get_y(1,10)
+    min_y = _get_y(0, 0) #obvious, I knowadd OrdinaryDiffEqTsit5
+    max_y = _get_y(1, 10)
     y_grid = vcat(LinRange(min_y, 100, 100), LinRange(100.1, max_y, 1000))
     F_grid = [_F(y) for y in y_grid]
     global F_interpolant = AkimaInterpolation(F_grid, y_grid)
-    y_grid = vcat(LinRange(min_y, 10., 10000), LinRange(10.1, max_y, 10000))
+    y_grid = vcat(LinRange(min_y, 10.0, 10000), LinRange(10.1, max_y, 10000))
     dFdy_grid = [_dFdy(y) for y in y_grid]
     global dFdy_interpolant = AkimaInterpolation(dFdy_grid, y_grid)
 end
